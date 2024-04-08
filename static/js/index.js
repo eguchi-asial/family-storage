2d397fbf8ed47e97e4583d1245b4758f99b0cb58
eguchi <eguchi@asial.co.jp>
2024-04-08 12:48:12 +0900

    編集ボタンと削除チェックボックスの実装

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var deleteButton = document.getElementById('delete-button');
-  
-    function updateDeleteButtonVisibility() {
-      var selectedThumbnails = document.querySelectorAll('.thumbnail.selected');
-      deleteButton.style.display = selectedThumbnails.length > 0 ? 'block' : 'none';
-    }
-  
-    // クリックで選択状態を切り替える
-    document.querySelectorAll('.thumbnail').forEach(function(thumbnail) {
-      thumbnail.addEventListener('click', function() {
-        this.classList.toggle('selected');
-        updateDeleteButtonVisibility();
-      });
+    const selectButton = document.getElementById('select-button');
+    const deleteButton = document.getElementById('delete-button');
+
+    selectButton.addEventListener('click', function() {
+        document.querySelectorAll('.thumbnail').forEach(function(thumbnail) {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.style.position = 'absolute';
+            checkbox.style.top = '5px';
+            checkbox.style.right = '5px';
+            checkbox.addEventListener('change', function() {
+                const thumbnail = this.closest('.thumbnail');
+                if (this.checked) {
+                    thumbnail.classList.add('selected');
+                } else {
+                    thumbnail.classList.remove('selected');
+                }
+            
+                const selectedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+                const deleteButton = document.getElementById('delete-button');
+                if (selectedCheckboxes.length > 0) {
+                    deleteButton.style.display = 'block';
+                } else {
+                    deleteButton.style.display = 'none';
+                }
+            });
+            thumbnail.appendChild(checkbox);
+        });
     });
   
     // 削除ボタンが押されたときの処理
     deleteButton.addEventListener('click', function() {
-      var selectedImages = Array.from(document.querySelectorAll('.thumbnail.selected')).map(function(thumbnail) {
+      const selectedImages = Array.from(document.querySelectorAll('.thumbnail.selected')).map(function(thumbnail) {
         return thumbnail.querySelector('img').alt;
       });
   
@@ -38,8 +53,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function checkFileSize(input) {
-    var totalFileSize = 0;
-    for (var i = 0; i < input.files.length; i++) {
+    let totalFileSize = 0;
+    for (let i = 0; i < input.files.length; i++) {
         totalFileSize += input.files[i].size; // size in bytes
     }
     // Convert size to MB
